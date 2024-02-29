9d910bc5a5a57f8b3391674d23e4ee1f893bdb7a
yellow-tshirt <mar.occhinegro@gmail.com>
2024-02-29 15:59:36 +0100

    added water

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod arrays;
-use crate::arrays::biggest_water_container::*;
+use crate::arrays::biggest_water_container::container_with_most_water_brute;
 
 fn main() {
-    let a = vec![1,2,3];
+    let a = vec![7,1,2,3,9];
+    let solution = container_with_most_water_brute(&a) ;
+    println!("{}", solution);
 }
