d191e39c9baeed14305033cb4c19c2c1e10ed83f
yellow-tshirt <mar.occhinegro@gmail.com>
2024-03-01 17:47:24 +0100

    more efficient biggest water container

--- src/arrays/biggest_water_container.rs
+++ src/arrays/biggest_water_container.rs
@@ -1,4 +1,3 @@
-
 pub fn container_with_most_water_brute(arr: &[i32]) -> i32{
     let mut max_area = 0;
     for i in 0..arr.len() {
@@ -8,6 +7,26 @@ pub fn container_with_most_water_brute(arr: &[i32]) -> i32{
                 max_area = area_of_container;
             }
         }
+    }
+    max_area
+}
+
+pub fn container_with_most_water(arr: &[i32]) -> i32{
+    let mut max_area = 0;
+    let mut left_pointer = 0;
+    let mut right_pointer = arr.len()-1;
+    let mut distance_between_pointers = right_pointer - left_pointer;
+    while distance_between_pointers >= 1{
+        let current_area = i32::min(arr[left_pointer], arr[right_pointer]) * distance_between_pointers as i32;
+        if current_area > max_area {
+            max_area =  current_area;
+        }
+        if arr[left_pointer] <= arr[right_pointer]{
+            left_pointer += 1;
+        }else{
+            right_pointer -= 1;
+        }
+        distance_between_pointers = right_pointer - left_pointer;
     }
     max_area
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod arrays;
-use crate::arrays::biggest_water_container::container_with_most_water_brute;
+use crate::arrays::biggest_water_container::container_with_most_water;
 
 fn main() {
-    let a = vec![7,1,2,3,9];
-    let solution = container_with_most_water_brute(&a) ;
+    let a = vec![7,1,2,100,99,9];
+    let solution = container_with_most_water(&a) ;
     println!("{}", solution);
 }
